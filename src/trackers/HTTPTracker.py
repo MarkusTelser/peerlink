eb4fc99d2b4f27335ec15e3d833ea0559d01ca5a
Markus Telser <markus.telser99@gmail.com>
2021-09-18 23:27:15 +0200

    refactor to oop class

--- src/trackers/HTTPTracker.py
+++ src/trackers/HTTPTracker.py
@@ -1,30 +1,11 @@
+import json
 
-def trackerHTTP(addr, info_hash, peer_id, port, uploaded, downloaded, left, ip=None, event=None, numwant=None, no_peer_id=None, compact=None):
-    params = ""
+from requests import get
+from bencode import decode
 
-    # required parameters
-    params += f"info_hash={info_hash}"
-    params += f"&peer_id={peer_id}"
-    params += f"&port={port}"
-    params += f"&uploaded={uploaded}"
-    params += f"&downloaded={downloaded}"
-    params += f"&left={left}"
-    
-    # optional parameters
-    if ip is not None:
-        params += f"&ip={ip}"
-    if event is not None:
-        params += f"&event={event}"
-    if numwant is not None:
-        params += f"&numwant={numwant}"
-    if no_peer_id is not None:
-        params += "&no_peer_id={no_peer_id}"
-    if compact is not None:
-        params += "&compact={compact}"
-
-    url = f"{addr}?{params}"
-    recv = requests.get(url)
-    
+class HTTPTracker:
+    def __init__(self, adr_url):
+        self.adr_url = adr_url
     """
     failure_code key that tracker might send back:
 
@@ -39,13 +20,51 @@ def trackerHTTP(addr, info_hash, peer_id, port, uploaded, downloaded, left, ip=N
     500	Client sent an eventless request before the specified time.
     900	Generic error.
     """
-    
+    def request(self, info_hash, peer_id, port, uploaded, downloaded, left, ip=None, event=None, numwant=None, no_peer_id=None, compact=None):
+        params = ""
 
-    print("full url: ", url)
-    answer = bencode.decode(recv.text)
-    print(json.dumps(answer, indent=4, sort_keys=True))
-    print(recv.status_code)
+        # required parameters
+        params += f"info_hash={info_hash}"
+        params += f"&peer_id={peer_id}"
+        params += f"&port={port}"
+        params += f"&uploaded={uploaded}"
+        params += f"&downloaded={downloaded}"
+        params += f"&left={left}"
 
-    recv.close()
-    
-    peers = answer.get("peers")
+        # optional parameters
+        if ip is not None:
+            params += f"&ip={ip}"
+        if event is not None:
+            params += f"&event={event}"
+        if numwant is not None:
+            params += f"&numwant={numwant}"
+        if no_peer_id is not None:
+            params += "&no_peer_id={no_peer_id}"
+        if compact is not None:
+            params += "&compact={compact}"
+
+        url = f"{self.adr_url}?{params}"
+        if "https" in self.adr_url:
+            payload = {
+                'info hash': info_hash, 
+                'peer_id': peer_id,
+                'port': port,
+                'uploaded': uploaded,
+                'downloaded' : downloaded,
+                'left' : left,
+            }
+            print("in")
+            recv = get(self.adr_url, data=payload)
+        else:
+            recv = get(url)
+        print(recv.text)
+        answer = decode(recv.text)
+        print(recv.text)
+        
+        #debug
+        print(json.dumps(answer, indent=4, sort_keys=True))
+        print(recv.status_code)
+
+        recv.close()
+
+        return answer
