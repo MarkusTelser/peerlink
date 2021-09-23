9bffd4a3a86dd326738604ce26a3309b41a2e927
Markus Telser <markus.telser99@gmail.com>
2021-09-23 17:12:15 +0200

    remove doc comment

--- src/TorrentParser.py
+++ src/TorrentParser.py
@@ -99,40 +99,6 @@ class TorrentParser:
 
     def get_files(self):
         pass
-    """
-    # extract data
-    announce = data["announce"]
-    con = announce.split("/")[0][:-1]
-    typ = announce.split("/")[3]
-    adr = announce.split("/")[2]
-    hasPortDefined = adr.find(":") != -1
-                                                      
-    # for trackers
-    uploaded = "0"
-    downloaded = "0"
-    left = "2000"
-    info_hash = getHashInfo(data)
-    port = int(adr.split(":")[1]) if hasPortDefined else 6969
-    ip = adr.split(":")[0] if hasPortDefined else adr
-    peer_id = ''.join(random.choice(string.ascii_letters) for _ in range(20))
-    
-    # debug
-    printTorrentFile(data)
-
-    if typ == "announce":
-        if con == "udp":
-            trackerUPS(ip, port)
-        if con == "http":
-            trackerHTTP(announce, info_hash, peer_id, port, uploaded, downloaded, left)
-
-            for i in range(3):
-                announce = data["announce-list"][i][0]
-                print(announce)
-                ip = announce.split("/")[2].split(":")[0]
-                port = announce.split("/")[2].split(":")[1]
-                print(ip, port)
-                trackerHTTP(announce, info_hash, peer_id, port, uploaded, downloaded, left)
-    """
 
 import os
 for file in os.listdir("../data/all"):
