6cbb74cf9ea7dee8da40e869a36e39c95b8e40b0
Markus Telser <markus.telser99@gmail.com>
2021-09-23 17:11:04 +0200

    fully evaluate torrent, for testing

--- src/TorrentParser.py
+++ src/TorrentParser.py
@@ -11,12 +11,13 @@ class TorrentParser:
         self.filepath = filepath
         self.data = None
 
-    def main():
+    def main(self):
         if self.evaluate():
             self.getData()
 
     def getData(self):
         if self.filepath != None:
+            print(self.filepath)
             if exists(self.filepath):
                 with open(self.filepath, "rb") as f:
                     encry = f.read()
@@ -33,14 +34,45 @@ class TorrentParser:
         if self.data == None:
             print("Error: No data loaded")
         try:
-            self.data["announce"]
-            self.data["info"]
+            keys = list(self.data.keys())
+            # some torrents dont follow standards and use announce-list only
+            if "announce" in self.data or "announce-list" in self.data:
+                if "announce" in self.data:
+                    keys.remove("announce")
+                if "announce-list" in self.data:
+                    keys.remove("announce-list")
+            else:
+                print("Error: Neither announce nor announce-list in torrent")
+            if "created by" in self.data:
+                keys.remove("created by")
+            if "creation date" in self.data:
+                keys.remove("creation date")
+            if "comment" in self.data:
+                keys.remove("comment")
+            if "encoding" in self.data:
+                keys.remove("encoding")
+            # peers for DHT
+            if "nodes" in self.data:
+                keys.remove("nodes")
+           
+            
+            if "info" in self.data:
+                del self.data.get("info")["pieces"]
+                print(self.data.get("info"))
+            else:
+                print("Error: Doesnt contain info in torrent")
+            
+            # print the remaining keys
+            for item in keys:
+                print(self.data[item])
+            print(keys)
             
         except KeyError:
             print("Error: Key doesn't exist")
 
     def getDebugInformation(self):
-        print("Announce: ", self.data['announce'])
+        print(json.dumps(self.data, indent=4, sort_keys=True))
+        #print("Announce: ", self.data['announce'])
         print("Info: ", self.data['info'])
         if "announce-list" in self.data:
             print("Announce-List: ", self.data["announce-list"][:3])
@@ -53,7 +85,8 @@ class TorrentParser:
         
         info = self.data
         del info["info"]["pieces"]
-        del info["announce-list"][3:]
+        if "announce-list" in info:
+            del info["announce-list"][3:]
         print(json.dumps(info, indent=4, sort_keys=True))
 
     def has_multi_file(self):
@@ -100,3 +133,10 @@ class TorrentParser:
                 print(ip, port)
                 trackerHTTP(announce, info_hash, peer_id, port, uploaded, downloaded, left)
     """
+
+import os
+for file in os.listdir("../data/all"):
+    filepath = os.path.join("../data/all", file)
+    parse = TorrentParser(filepath=filepath)
+    parse.getData()
+    parse.evaluate()
