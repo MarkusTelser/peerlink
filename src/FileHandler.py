0d3b4a9f51a200eeab5b8d0df6a3a982e1c13f87
Markus Telser <markus.telser99@gmail.com>
2021-11-14 21:39:53 +0100

    implement FileHandler

--- src/FileHandler.py
+++ src/FileHandler.py
@@ -1,9 +1,27 @@
-class FileHandler:
-    def write_to_file():
-        pass
+from hashlib import sha1
+from threading import RLock
 
-    def check_hash():
-        pass
+class FileHandler:
+    def __init__(self, data, path):
+        self.lock = RLock()
+        self.data = data
+        self.path = path
+
+    def write_block(self, id, start, data):
+        self.lock.acquire()
+        with open(self.path, 'rb+') as f:
+            f.seek(self.data.piece_length * id + start)
+            f.write(data)
+        self.lock.release()
+
+    def check_hash(self, id, hash):
+        with open(self.path, "rb") as f:
+            f.seek()
+            data = f.read(self.data.piece_length)
+
+        file_hash = sha1(data).digest()
+        return hash == file_hash
+        
 
     def check_all_hashes():
         pass
