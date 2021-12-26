09d3339aef8ac4e5e60de592cb3a38f33fc99601
Markus Telser <markus.telser99@gmail.com>
2021-12-26 17:58:14 +0100

    finish side panel gui

--- src/frontend/widgets/SidePanel.py
+++ src/frontend/widgets/SidePanel.py
@@ -1,14 +1,18 @@
+from PyQt6 import QtCore
 from PyQt6.QtCore import QModelIndex
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QTabWidget, QVBoxLayout, QWidget
 
 
-from PyQt6.QtWidgets import QTabWidget, QSizePolicy, QTreeWidget, QTreeWidgetItem
+from PyQt6.QtWidgets import QTabWidget, QSizePolicy, QTreeWidget, QTreeWidgetItem, QFrame
 
 class OtherTab(QWidget):
     def __init__(self):
         super().__init__()
 
 class FilterTab(QWidget):
+    changed_item = QtCore.pyqtSignal(str)
+    
     def __init__(self):
         super().__init__()
         
@@ -17,33 +21,79 @@ class FilterTab(QWidget):
         
         data = {
             'Torrents' : [
-                'Active',
-                'Inactive',
-                'Downloading',
-                'Seeding',
-                'Leeching'
+                ('All', 'resources/all.svg'),
+                ('Active', 'resources/active.svg'),
+                ('Inactive', 'resources/inactive.svg'),
+                ('Downloading', 'resources/downloading.svg'),
+                ('Leeching', 'resources/leeching.svg'),
+                ('Seeding', 'resources/seeding.svg'),
+                ('Overseeding', 'resources/overseeding.svg'),
+                ('Stalling', 'resources/stalling.svg'),
+                ('Download Stalling', 'resources/stalling_download.svg'),
+                ('Upload Stalling', 'resources/stalling_upload.svg')
             ],
             'Trackers': [
-                'Working',
-                'Unreachable'
-                'Error',
-                'Warnings'
+                ('Working', 'resources/working.svg'),
+                ('Unreachable', 'resources/unreachable.svg'),
+                ('Error', 'resources/error.svg'),
+                ('Warning', 'resources/warning.svg')
             ]
         }
         
-        tab_tree = QTreeWidget()
-        #tab_tree.setIndentation(0)
+        self.tab_tree = QTreeWidget()
+        self.tab_tree.itemSelectionChanged.connect(self.item_selected)
+        self.tab_tree.setStyleSheet("""                  
+        QTreeView{
+            border: 0px solid white;
+            show-decoration-selected: 0;
+        }
+        QTreeView::item:selected {
+            border: 1px solid green;
+            background: rgb(0,77,0);
+        }
+
+        QTreeView::branch:has-children:!has-siblings:closed,
+        QTreeView::branch:closed:has-children:has-siblings {
+                border-image: none;
+                image: url(resources/branch-closed.png);
+        }
+
+        QTreeView::branch:open:has-children:!has-siblings,
+        QTreeView::branch:open:has-children:has-siblings  {
+                border-image: none;
+                image: url(resources/branch-open.png);
+        }
+        """)
+        self.tab_tree.setFrameShadow(QFrame.Shadow.Raised)
+        self.tab_tree.header().hide()
         
         for key in data.keys(): 
-            category = QTreeWidgetItem(tab_tree)
+            category = QTreeWidgetItem(self.tab_tree)
             category.setText(0, key)
             
             for item in data[key]:
+                name, icon = item
                 child = QTreeWidgetItem()
-                child.setText(0, item)
+                child.setText(0, name)
+                child.setIcon(0, QIcon(icon))
                 category.addChild(child)
         
-        layout.addWidget(tab_tree)        
+        self.tab_tree.expandAll()
+        layout.addWidget(self.tab_tree)
+        
+    def item_selected(self):
+        filter_string = ""
+        for item in self.tab_tree.selectedItems():
+            if item.parent() != None:
+                filter_string += item.parent().text(0)
+                filter_string += f"/{item.text(0)}"
+            else:
+                item.setSelected(False)
+                filter_string += item.text(0)
+        self.changed_item.emit(filter_string)
+    
+    def mouse_pressed():
+        pass
         
 
 class SidePanel(QTabWidget):
