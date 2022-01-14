9700e60abbc58c145ab2b0a65536f5af36480207
Markus Telser <markus.telser99@gmail.com>
2022-01-14 11:03:35 +0100

    add chart tab, _update + _clear for every tab

--- src/frontend/views/TorrentDetailView.py
+++ src/frontend/views/TorrentDetailView.py
@@ -1,7 +1,8 @@
-from PyQt6.QtWidgets import QTabWidget, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem
-from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QHeaderView, QTabWidget, QTableView, QWidget, QVBoxLayout
+from PyQt6.QtGui import QColor, QIcon, QPainter, QStandardItem, QStandardItemModel
+from PyQt6.QtCharts import QChart, QChartView, QSplineSeries, QValueAxis
 from src.backend.metadata.TorrentData import TorrentFile
-
 from src.backend.swarm import Swarm
 from src.frontend.models.TorrentTreeModel import TorrentTreeModel
 from src.frontend.views.TorrentTreeView import TorrentTreeView
@@ -10,10 +11,87 @@ class GeneralTab(QWidget):
     def __init__(self):
         super().__init__()
  
+class ChartTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        main_layout = QVBoxLayout()
+        self.setLayout(main_layout)
+        
+        series = QSplineSeries()
+        series.append(10, 1)
+        series.append(5, 3)
+        series.append(8, 4)
+        series.append(9, 6)
+        series.append(2, 9)
+        
+        chart = QChart()
+        chart.legend().hide()
+        chart.createDefaultAxes()
+        chart.layout().setContentsMargins(0, 0, 0, 0)
+        chart.setTheme(QChart.ChartTheme.ChartThemeBlueIcy)
+        chart.setBackgroundRoundness(0)
+        chart.addSeries(series)
+        
+        speed_axis = QValueAxis()
+        speed_axis.setRange(0, 10)
+        speed_axis.setTickCount(5)
+        speed_axis.setTitleText("speed axis")
+        chart.addAxis(speed_axis, Qt.AlignmentFlag.AlignLeft)
+        
+        time_axis = QValueAxis()
+        time_axis.setRange(0, 10)
+        time_axis.setTickCount(0)
+        time_axis.setTitleText("time axis")
+        chart.addAxis(time_axis, Qt.AlignmentFlag.AlignBottom)
+        
+        chart_view = QChartView(chart)
+        chart_view.setBackgroundBrush(QColor("red"))
+        chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
+        
+        main_layout.addWidget(chart_view)
+ 
 class TrackersTab(QWidget):
     def __init__(self):
         super().__init__()    
         
+        main_layout = QVBoxLayout()
+        self.setLayout(main_layout)
+        
+        self.table_widget = QTableView()
+        self.model = QStandardItemModel()
+        self.table_widget.setModel(self.model)
+        
+        #self.table_widget.verticalHeader().hide()
+        self.table_widget.setSortingEnabled(True)
+        self.table_widget.horizontalHeader().setStretchLastSection(True)
+        self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+        self.table_widget.setStyleSheet("background: black")
+        main_layout.addWidget(self.table_widget)
+        self.setStyleSheet("background: green")
+        
+        self._clear()
+    
+    def _update(self, tracker_list: list = []):
+        self._clear()
+        for tracker in tracker_list:
+            a = tracker.address if type(tracker.address) == str else f'{tracker.address[0]}:{tracker.address[1]}'
+            addr = QStandardItem(a)
+            states = ['no contact', 'connecting...', 'finished', 'stopped with error']
+            colors = [QColor('black'), QColor('yellow'), QColor('green'), QColor('red')]
+            status = QStandardItem(states[tracker.status.value])
+            status.setForeground(colors[tracker.status.value])
+            error = QStandardItem(tracker.error)
+            peers = QStandardItem(str(len(tracker.peers)))
+            leechers = QStandardItem(str(tracker.leechers))
+            seeders = QStandardItem(str(tracker.seeders))
+            self.model.appendRow([addr, status, peers, leechers, seeders, error])
+    
+    def _clear(self):
+        self.model.clear()
+        headers = ["address", "status", "peers", "leechers", "seeders", "error"]
+        self.model.setHorizontalHeaderLabels(headers)
+    
 class PeersTab(QWidget):
     def __init__(self):
         super().__init__()   
@@ -21,18 +99,37 @@ class PeersTab(QWidget):
         main_layout = QVBoxLayout()
         self.setLayout(main_layout)
         
-        table_widget = QTableWidget()
-        table_widget.setColumnCount(4)
-        table_widget.setRowCount(5)
-        headers = ["ip", "port", "connection type", "status"]
-        table_widget.setHorizontalHeaderLabels(headers)
-        table_widget.verticalHeader().hide()
-        table_widget.horizontalHeader().setStretchLastSection(True)
-        table_widget.setItem(0, 1, QTableWidgetItem("data"))
-        table_widget.setStyleSheet("background: green;")
-        main_layout.addWidget(table_widget)
+        self.table_widget = QTableView()
+        self.model = QStandardItemModel()
+        self.table_widget.setModel(self.model)
         
+        self.table_widget.setSortingEnabled(True)
+        self.table_widget.verticalHeader().hide()
+        self.table_widget.horizontalHeader().setStretchLastSection(True)
+        self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+        self.table_widget.setStyleSheet("background: green;")
+        main_layout.addWidget(self.table_widget)
         self.setStyleSheet("background: purple")
+        
+        self._clear()
+    
+    def _update(self, peer_list: list = []):
+        self._clear()
+        for peer in peer_list:
+            ip = QStandardItem(peer.address[0])
+            port = QStandardItem(peer.address[1])
+            con_type = QStandardItem('')
+            status = QStandardItem('connecting...')
+            ip.setEditable(False)
+            port.setEditable(False)
+            con_type.setEditable(False)
+            status.setEditable(False)
+            self.model.appendRow([ip, port, con_type, status])
+    
+    def _clear(self):
+        self.model.clear()
+        headers = ["ip", "port", "connection type", "status"]
+        self.model.setHorizontalHeaderLabels(headers)
     
 class FilesTab(QWidget):
     def __init__(self):
@@ -47,24 +144,38 @@ class FilesTab(QWidget):
         self.tree_view.setStyleSheet("background: green;")
         main_layout.addWidget(self.tree_view)
         
-    def _update(self, data: TorrentFile):
+    def _update(self, data: TorrentFile = None):
         self.model.update(data)
+    
+    def _clear(self):
+        self.model.removeRow(0)
+        self.tree_view.reevaluate()
 
 class TorrentDetailView(QTabWidget):
     def __init__(self):
         super().__init__()
         
         self.general_tab = GeneralTab()
+        self.chart_tab = ChartTab()
         self.trackers_tab = TrackersTab()
         self.peers_tab = PeersTab()
         self.files_tab = FilesTab()
         
         self.addTab(self.general_tab, QIcon('resources/general.svg'), "General")
+        self.addTab(self.chart_tab, QIcon('resources/chart.svg'), "Chart")
         self.addTab(self.trackers_tab, QIcon('resources/trackers.svg'), "Trackers")
         self.addTab(self.peers_tab, QIcon('resources/peer.svg'), "Peers")
         self.addTab(self.files_tab, QIcon('resources/files.svg'), "Files")
         
+        self.setMovable(True)
         self.setStyleSheet("background-color: blue;")
     
     def _update(self, dt: Swarm):
+        self.trackers_tab._update(dt.tracker_list)
+        self.peers_tab._update(dt.peer_list)
         self.files_tab._update(dt.data.files)
+    
+    def _clear(self):
+        self.trackers_tab._clear()
+        self.peers_tab._clear()
+        self.files_tab._clear()
