aad7afe8d5487de45422dd30b1e9ff5f30877ccc
Markus Telser <markus.telser99@gmail.com>
2022-03-10 12:11:09 +0100

    added basic seeding server

---
+++ src/backend/peer_protocol/TCPServer.py
@@ -0,0 +1,40 @@
+import asyncio
+from asyncio import StreamReader, StreamWriter
+
+class TCPServer:
+    PORT_RANGE = range(6881, 6889 + 1)
+
+    def __init__(self):
+        self._server = None
+        self.port = None
+
+
+    async def start(self):
+        for port in TCPServer.PORT_RANGE:
+            try:
+                self._server = await asyncio.start_server(self.client_connected, host="0.0.0.0", port=port)
+            except Exception as e:
+                # TODO log
+                print(e)
+            else:
+                self.port = port
+                print('WORKING ON PORT', port)
+                break
+
+    async def stop(self):
+        if self._server == None:
+            return
+        
+        self._server.close()
+        await self._server.wait_closed()
+    
+    def client_connected(self, reader: StreamReader, writer: StreamWriter):
+        print('\033[91m' + 'CLIENT CONNECTED' * 100 + '\033[0m')
+
+
+if __name__ == '__main__':
+    async def run():
+        server = TCPServer()
+        await server.start()
+
+    asyncio.run(run())
