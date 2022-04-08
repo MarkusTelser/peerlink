3ba6f8b2440ccf8e3f5c57a1538ada9af65ac90c
Markus Telser <markus.telser99@gmail.com>
2022-04-08 21:11:19 +0200

    restruct resouces, add languages ts files

---
+++ resources/languages/qt_de.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="la_VA">
+<context>
+    <name>LaunchWindow</name>
+    <message>
+        <source>Launch Window - PeerLink</source>
+        <translation>Startfenster - PeerLink</translation>
+    </message>
+    <message>
+        <source> open &amp;torrent file</source>
+        <translation> öffne &amp;Torrent Datei</translation>
+    </message>
+    <message>
+        <source> add &amp;magnet link</source>
+        <translation> füge &amp;Magnet Link hinzu</translation>
+    </message>
+    <message>
+        <source>+ drag and drop support</source>
+        <translation>+ Drag und Drop Unterstützung</translation>
+    </message>
+    <message>
+        <source>Drag and Drop
+files here</source>
+        <translation>Drag und Drop&lt;br/&gt;Dateien hier</translation>
+    </message>
+    <message>
+        <source>Wrong file format!</source>
+        <translation>Falsches Dateiformat!</translation>
+    </message>
+    <message>
+        <source>don&apos;t show again</source>
+        <translation>nicht mehr anzeigen</translation>
+    </message>
+</context>
+<context>
+    <name>FileDialog</name>
+    <message>
+        <source>Open Torrent File - PeerLink</source>
+        <translation>Öffne Torrent Datei - PeerLink</translation>
+    </message>
+    <message>
+        <source>Torrent files (*.torrent)</source>
+        <translation>Torrent Dateien (*.torrent)</translation>
+    </message>
+</context>
+<context>
+    <name>MagnetLinkDialog</name>
+    <message>
+        <source>Add Magnet Link - PeerLink</source>
+        <translation>Hinzufügen Magnet Link - PeerLink</translation>
+    </message>
+    <message>
+        <source>Enter magnet link:</source>
+        <translation>Geben Sie den Magnet Link ein:</translation>
+    </message>
+    <message>
+        <source>No magnet link inserted!</source>
+        <translation>Kein Magnet Link eingegeben!</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+</context>
+<context>
+    <name>PreviewWindow</name>
+    <message>
+        <source>View Torrent - PeerLink</source>
+        <translation>Zeige Torrent- PeerLink</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Pfad</translation>
+    </message>
+    <message>
+        <source>Enter download path</source>
+        <translation>Gebe Downloadpfad ein</translation>
+    </message>
+    <message>
+        <source>select folder</source>
+        <translation>wähle Ordner aus</translation>
+    </message>
+    <message>
+        <source>set as default path</source>
+        <translation>als Standardpfad festlegen</translation>
+    </message>
+    <message>
+        <source>Enter new/existing category</source>
+        <translation>Gebe neue/existierende Kategorie ein</translation>
+    </message>
+    <message>
+        <source>set as default category</source>
+        <translation>als Standardkategorie festlegen</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Optionen</translation>
+    </message>
+    <message>
+        <source>Download Strategy:</source>
+        <translation>Download Strategie:</translation>
+    </message>
+    <message>
+        <source>start immediately</source>
+        <translation>sofort starten</translation>
+    </message>
+    <message>
+        <source>check hashes</source>
+        <translation>überprüfe Hashwerte</translation>
+    </message>
+    <message>
+        <source>pre pad empty files</source>
+        <translation>fülle Dateien auf</translation>
+    </message>
+    <message>
+        <source>Torrent Information</source>
+        <translation>Torrent Informationen</translation>
+    </message>
+    <message>
+        <source>Size:</source>
+        <translation>Größe:</translation>
+    </message>
+    <message>
+        <source>Creation date:</source>
+        <translation>Erstellungsdatum:</translation>
+    </message>
+    <message>
+        <source>Created by:</source>
+        <translation>Erstellt von:</translation>
+    </message>
+    <message>
+        <source>Comment:</source>
+        <translation>Kommentar:</translation>
+    </message>
+    <message>
+        <source>Hash:</source>
+        <translation>Hashwert:</translation>
+    </message>
+    <message>
+        <source>of {} on local disk</source>
+        <translation>von {} auf lokaler Festplatte</translation>
+    </message>
+    <message>
+        <source>Select Directory - PeerLink</source>
+        <translation>Wähle Ordner - PeerLink</translation>
+    </message>
+    <message>
+        <source>Save path is empty!</source>
+        <translation>Speicherpfad ist leer!</translation>
+    </message>
+    <message>
+        <source>Save path does not exist!</source>
+        <translation>Speicherpfad existiert nicht!</translation>
+    </message>
+    <message>
+        <source>Save path is not a directory!</source>
+        <translation>Speicherpfad ist kein Ordner!</translation>
+    </message>
+    <message>
+        <source>don&apos;t show again</source>
+        <translation>nicht mehr anzeigen</translation>
+    </message>
+    <message>
+        <source>rarest first (default)</source>
+        <translation>seltenste zuerst (Standard)</translation>
+    </message>
+    <message>
+        <source>sequential</source>
+        <translation>sequentiell</translation>
+    </message>
+    <message>
+        <source>random</source>
+        <translation>zufällig</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorDialog</name>
+    <message>
+        <source>Error - PeerLink</source>
+        <translation>Fehler - PeerLink</translation>
+    </message>
+</context>
+<context>
+    <name>FileTreeModel</name>
+    <message>
+        <source>name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <source>size</source>
+        <translation>Größe</translation>
+    </message>
+</context>
+</TS>
